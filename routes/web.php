4b798d35aa2dabf2c7cd943fb33d69b9c616516f
star <star@you-x.ru>
2025-06-02 11:03:09 +0300

    second commit

--- routes/web.php
+++ routes/web.php
@@ -7,10 +7,4 @@ use Illuminate\Support\Facades\Route;
 //    return view('welcome');
 //});
 Route::get('/', [FilesController::class, 'index'])->name('index');
-//Route::get('/{dir?}', [FilesController::class, 'directory'])->name('directory');
-
-//Route::get('/directory/{dir?}', [FilesController::class, 'directory'])
-//    ->where('path', '[a-zA-Z0-9/_-]+') // чтобы путь мог содержать слэши
-//    ->name('directory');
-
 Route::any('/directory/{dir?}', [FilesController::class, 'directory'])->where('dir', '.*')->name('directory');
